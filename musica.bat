edb67a1eab8706f34497647bdc1784334f8ac178
Kayqne <73949134+Kayqne@users.noreply.github.com>
2025-11-05 09:46:15 -0300

    Refactor musica.bat for improved readability

--- musica.bat
+++ musica.bat
@@ -1,14 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Pergunta o link da playlist ou vídeo
 set /p URL=Digite a URL do video ou playlist do YouTube: 
 
-:: Pergunta a pasta de destino (padrão C:\Minhas Musicas)
 set /p DESTINO=Digite a pasta onde quer salvar (pressione Enter para padrão C:\Minhas Musicas): 
 if "!DESTINO!"=="" set DESTINO=C:\Minhas Musicas
 
-:: Caminho do FFmpeg (ajuste se precisar)
 set FFMPEG_PATH=C:\ffmpeg\bin
 
 echo.
@@ -19,3 +16,4 @@ echo.
 yt-dlp -x --audio-format mp3 --audio-quality 0 --ffmpeg-location "!FFMPEG_PATH!" --continue --ignore-errors -o "!DESTINO!\%%(playlist_index)s - %%(title)s.%%(ext)s" "!URL!"
 
 pause
+
